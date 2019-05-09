47bd835b089424ec00fd50e390c9e3b3284c5c84
Gabe Johnson <gabe@blankslatesystems.com>
2019-05-09 09:30:10 -0600

    use <Path>, made separate component

--- src/drawing/draw.tsx
+++ src/drawing/draw.tsx
@@ -1,56 +1,20 @@
 import React from 'react';
-import Konva from 'konva';
-import { Stage, Layer, Star, Text } from 'react-konva';
-import { KonvaEventObject } from 'konva/types/types';
-
-
-const handleDragStart = (e: KonvaEventObject<DragEvent>) => {
-  if (e == null || e.target == null ) return;
-  e.target.setAttrs({
-    shadowOffset: {
-      x: 15,
-      y: 15
-    },
-    scaleX: 1.1,
-    scaleY: 1.1
-  });
-};
-
-const handleDragEnd = (e: KonvaEventObject<DragEvent>) => {
-  e.target.to({
-    duration: 0.5,
-    easing: Konva.Easings.ElasticEaseOut,
-    scaleX: 1,
-    scaleY: 1,
-    shadowOffsetX: 5,
-    shadowOffsetY: 5
-  });
-};
+import { Stage, Layer, Path } from 'react-konva';
 
 export const Draw: React.FC = () => {
   return (      
   <Stage width={window.innerWidth} height={window.innerHeight}>
     <Layer>
-      <Text text="Try to drag a star" />
-      {[...Array(10).keys()].map(i => (
-        <Star
-          key={i}
-          x={Math.random() * window.innerWidth}
-          y={Math.random() * window.innerHeight}
-          numPoints={5}
-          innerRadius={20}
-          outerRadius={40}
-          fill="#89b717"
-          opacity={0.8}
-          draggable
-          rotation={Math.random() * 180}
-          shadowColor="black"
-          shadowBlur={10}
-          shadowOpacity={0.6}
-          onDragStart={handleDragStart}
-          onDragEnd={handleDragEnd}
-        />
-      ))}
+      <RewindIcon />
     </Layer>
   </Stage>);
+}
+
+const RewindIcon: React.FC = () => {
+  return (      <Path
+    x= {240}
+    y= {40}
+    data={'M12.582,9.551C3.251,16.237,0.921,29.021,7.08,38.564l-2.36,1.689l4.893,2.262l4.893,2.262l-0.568-5.36l-0.567-5.359l-2.365,1.694c-4.657-7.375-2.83-17.185,4.352-22.33c7.451-5.338,17.817-3.625,23.156,3.824c5.337,7.449,3.625,17.813-3.821,23.152l2.857,3.988c9.617-6.893,11.827-20.277,4.935-29.896C35.591,4.87,22.204,2.658,12.582,9.551z'}
+    fill= 'green'
+  />);
 }
