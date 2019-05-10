cacf2449e788c1147207b86f9f9c2cd408c6a15e
Gabe Johnson <gabe@blankslatesystems.com>
2019-05-10 09:25:02 -0600

    more/better linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
+        'eslint:recommended',
         'plugin:react/recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended',
+        'prettier',
     ],
     parserOptions: {
         ecmaVersion: 2018, // expect to read modern JS syntax
@@ -12,14 +14,21 @@ module.exports = {
             jsx: true,
         },
     },
+    plugins: ['react', 'react-hooks'],
     env: {
         browser: true,
         node: true,
     },
     rules: {
         'react/prop-types': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-member-accessibility': 'off',
-        '@typescript-eslint/indent': ['error', 2],
+        '@typescript-eslint/indent': 'off', // conflicts w prettier
+        '@typescript-eslint/member-delimiter-style': 'off', // conflicts w prettier
+        '@typescript-eslint/type-annotation-spacing': 'off', // conflicts w prettier
+        'no-console': 'warn',
+        'prettier/prettier': 'warn',
     },
 };
